09abccd28ce411289988e8b6667a8cbf0f184178
Eddie Arevalo <eddiesarevalo@gmail.com>
2024-04-04 10:31:02 -0700

    Show Multiple Buckets
    -Added code from another project that allows viewing multiple buckets.

--- src/js/main.js
+++ src/js/main.js
@@ -16,11 +16,29 @@ import * as bootstrap from 'bootstrap';
 import './utils';
 import './error';
 
+import { missions } from './missions.js';
+
 // Show the <main>
 $("main").show();
 
+if (location.pathname == "/"){ 
+    $.each( missions, function( key, value ) {
+        var items = [];
+        $( ".bucketlist").append("<tr class='even'> <th scope='row'><div class='position-relative'><i class='fa-fw fas fa-folder' title='Directory' aria-hidden='true'></i><span class='sr-only'>(Directory)</span><a href='" + value.Path + "' class='stretched-link'></a></div></th><td class='name'> <div class='position-relative'> <a href='" + value.Path + "' class='stretched-link'>" + key + "</a></div></td></tr>" );
+        });
+}
+
+
+var SUBDIRS = [];
+$.each( missions, function( key, value ) {
+    SUBDIRS.push(value.Path);
+});
+
+
 import getS3Data from './list';
 $(function() {
-    getS3Data();
+    if (SUBDIRS.some(w => location.pathname.includes(w))) {
+        getS3Data();
+    }
 });
 
---
+++ src/js/missions.js
@@ -0,0 +1,15 @@
+export const missions = {
+    "pds": {
+        "Path": "/pds/",
+        "URL": "https://d1z62tir4fw0q0.cloudfront.net"
+    },
+    "noaa" : {
+        "Path": "/noaa/",
+        "URL": "https://noaa-bathymetry-pds.s3.us-east-1.amazonaws.com"
+    }
+  };
+
+export const exclude_prefixes = [
+    "mars_mro_ctx_globalmosaicv01_dickson_2023/", 
+    "THEMIS/USA_NASA_PDS_ODTGEO_100XX/"
+];
