94c000c089815c1a597433f1ea1ef2fbae67d90e
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2025-04-29 10:51:27 -0700

    Sanitize inputs to avoid possible vulnerability
    Resolves #136

--- src/js/error.js
+++ src/js/error.js
@@ -1,2 +1,11 @@
 // Configure the Back button href
-$(".card a.btn").attr("href", document.referrer);
+const sanitizeUrl = (url) => {
+  try {
+    const parsed = new URL(url);
+    return parsed.origin + parsed.pathname;
+  } catch (e) {
+    return '/';
+  }
+};
+
+$(".card a.btn").attr("href", sanitizeUrl(document.referrer));
